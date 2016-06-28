4190ff4146d5f164bfd1532e93fc539a87d9b666
Erik Campobadal <ConsoleTVs@gmail.com>
2016-06-28 02:10:51 +0200

    Added IP function

--- app/Http/Controllers/Admin/Laralum.php
+++ app/Http/Controllers/Admin/Laralum.php
@@ -70,5 +70,24 @@ class Laralum extends Controller
     {
         return Settings::first();
     }
+    
+    public static function getIP()
+    {
+        # Get Real IP
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+        {
+          $ip=$_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+        {
+          $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else
+        {
+          $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        
+        return $ip;
+    }
 
 }
